32a57fb3e97b94a47a0d5dccd9462713af8dca3c
joao.andrade <joaovandrade30@gmail.com>
2022-05-30 18:42:26 -0300

    Task #3 - FakerRest
    - Mapeamento de requisições Activities

---
+++ cypress/support/requests/fakerAPI/Activities.js
@@ -0,0 +1,49 @@
+/// <reference types="cypress" />
+
+
+class Activities {
+    getActivities(){
+        return cy.request({
+            method: "GET",
+            url: `${Cypress.env("FakeRESTApi")}/Activities`,
+            failOnStatusCode: false
+        })
+    }
+
+    postActivities(payload){
+        return cy.request({
+            method: "POST",
+            url: `${Cypress.env("FakeRESTApi")}/Activities`,
+            failOnStatusCode: false,
+            body: payload
+        })
+    }
+
+    getActivitiesId(id){
+        return cy.request({
+            method: "GET",
+            url: `${Cypress.env("FakeRESTApi")}/Activities/${id}`,
+            failOnStatusCode: false
+        })
+    }
+
+    putActivities(id, payload){
+        return cy.request({
+            method: "PUT",
+            url: `${Cypress.env("FakeRESTApi")}/Activities/${id}`,
+            failOnStatusCode: false,
+            body: payload
+        })
+    }
+
+    deleteActivities(id){
+        return cy.request({
+            method: "DELETE",
+            url: `${Cypress.env("FakeRESTApi")}/Activities/${id}`,
+            failOnStatusCode: false
+        })
+    }
+}
+
+
+export default new Activities
