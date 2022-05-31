b85ac68041f96f7835c78c69ba1826b3bd65b1c7
Jorge Patricio <65610111+JorgePatricio@users.noreply.github.com>
2022-05-31 09:50:58 -0300

    Task #8
    Feito as requisições do endpoint Accounts

---
+++ cypress/support/requests/bookStoreAPI/Accounts.js
@@ -0,0 +1,49 @@
+
+
+
+class Accounts {
+	postAccountsAuthorized(payload) {
+		return cy.request({
+			method: 'POST',
+			url: `${Cypress.env("BookStoreAPI")}/Authorized`,
+			failOnStatusCode: false,
+			body: payload
+		})
+	}
+
+	postAccountsToken(payload) {
+		return cy.request({
+			method: 'POST',
+			url: `${Cypress.env("BookStoreAPI")}/GenerateToken`,
+			failOnStatusCode: false,
+			body: payload
+		})
+	}
+
+	postAccountsUser(payload) {
+		return cy.request({
+			method: 'POST',
+			url: `${Cypress.env("BookStoreAPI")}/User`,
+			failOnStatusCode: false,
+			body: payload
+		})
+	}
+
+	deleteAccounts(userId) {
+		return cy.request({
+			method: 'DELETE',
+			url: `${Cypress.env("BookStoreAPI")}/User/${userId}`,
+			failOnStatusCode: false
+		})
+	}
+
+	getAccounts(userId) {
+		return cy.request({
+			method: 'GET',
+			url: `${Cypress.env("BookStoreAPI")}/User/${userId}`,
+			failOnStatusCode: false
+		})
+	}
+}
+
+export default new Accounts
